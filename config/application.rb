89b9b4c75faa01376608fc7239a6a6dbe60b3063
gregory <greg2502@gmail.com>
2014-05-01 14:39:00 +0200

    fix array bug with diff version

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,10 @@ require 'redis'
 REDIS = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 0)
 
 [
-  './app/{presenters,models,helpers,controllers}/**/*.rb',
+  './app/presenters/**/*.rb',
+  './app/models/**/*.rb',
+  './app/helpers/**/*.rb',
+  './app/controllers/**/*.rb',
   './{lib,api}/**/*.rb'
-].each{ |path| Dir.glob(path, &method(:require)) }
+].each{ |path| Dir.glob(path).sort.each(&method(:require)) }
 
